1f1129ca0d781bccd9e69336dde78f38dbbeb9d7
Arka Halder <105145575+arkachego@users.noreply.github.com>
2025-07-03 17:37:31 +0530

    Token is now also being retrieved from auth header

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -9,7 +9,10 @@ const validatorRouter = Router();
 
 validatorRouter.use(async (req, res, next) => {
   try {
-    const token = req.cookies?.['x-auth-token'] || null;
+    let token = req.cookies?.['x-auth-token'] || null;
+    if (!token && req.headerss?.['Authorization']) {
+      token = req.headerss?.['Authorization'].split(' ')[1];
+    }
     if (!token) {
       throw HttpError(403);
     }
